988ccf8234bc55078c911a54e3ea1f30be11bf84
isaacrudy <isaac.rudy@gmail.com>
2023-03-23 13:30:39 -0700

    Installed Breeze modules

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\NewslettersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,16 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('newsletters', [NewslettersController::class, 'index'])->name('newsletters.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('newsletters/create', [NewslettersController::class, 'create'])->name('newsletters.create');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::post('newsletters/store', [NewslettersController::class, 'store'])->name('newsletters.store');
-
-Route::get('newsletters/show/{id}', [NewslettersController::class, 'show'])->name('newsletters.show');
-
-Route::get('newsletters/edit/{id}', [NewslettersController::class, 'edit'])->name('newsletters.edit');
-
-Route::put('newsletters/update', [NewslettersController::class, 'update'])->name('newsletters.update');
-
-Route::get('newsletters/destroy/{id}', [NewslettersController::class, 'destroy'])->name('newsletters.destroy');
+require __DIR__.'/auth.php';
